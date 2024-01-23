a71477940e3652080af01b118920bae851778253
twaters27 <144744407+twaters27@users.noreply.github.com>
2024-01-22 20:53:17 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,53 @@
+const accountContainer = document.getElementById("create-account-container");
+const loginContainer = document.getElementById("login-container");
+const goToMake = document.getElementById("goToMake");
+const goToLogin = document.getElementById("goToLogin");
+
+
+function login() {
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
+  
+    var username = usernameInput.value;
+    var password = passwordInput.value;
+  
+    // Check if the provided username and password match
+    if (checkCredentials(username, password)) {
+      loginContainer.style.display = "none";
+      accountContainer.style.display = "none";
+      
+    } else {
+      alert("Incorrect Password or Username, Please try again.");
+    }
+  }
+  
+  function createAccount() {
+    var newUsernameInput = document.getElementById("new-username");
+    var newPasswordInput = document.getElementById("new-password");
+  
+    var newUsername = newUsernameInput.value;
+    var newPassword = newPasswordInput.value;
+  
+    // Check if the username already exists
+    if (localStorage.getItem(newUsername)) {
+      alert("Username already exists. Please choose a different one.");
+    } else {
+      // Save the new account in localStorage
+      localStorage.setItem(newUsername, newPassword);
+      alert("Account created successfully! You can now log in.");
+    }
+  }
+  
+  function checkCredentials(username, password) {
+    // Check if the provided username and password match any in localStorage
+    return localStorage.getItem(username) === password;
+  }
+  
+  goToMake.addEventListener("click", function () {
+    loginContainer.style.display = "none";
+    accountContainer.style.display = "block";
+  });
+  goToLogin.addEventListener("click", function () {
+    accountContainer.style.display = "none";
+    loginContainer.style.display = "block";
+  });
