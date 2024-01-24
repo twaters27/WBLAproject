aaa7d3538819fcab7db4615a6c1747a7a0d1a013
Myles Marshall <mmarsh319@west-mec.org>
2024-01-23 17:28:39 -0700

    many changes again

---
+++ cart.js
@@ -0,0 +1,19 @@
+let kbPrice = 15;
+let jrPrice = 10;
+let ppPrice = 12;
+let dbPrice = 20;
+let rsPrice = 40;
+let pbPrice = 90;
+let sudPrice = 100;
+let hhPrice = 7;
+let hbPrice = 35;
+let pubPrice = 45;
+let hfPrice = 15;
+let gbPrice = 50;
+let ybPrice = 10;
+let rbPrice = 15;
+let bbgPrice = 20;
+let ymPrice = 15;
+
+let totalCartPrice = 0;
+
